3e5f160eeee139bbec92a9d9a9c483eecf4ec102
tejuamirthi <tejuamirthi@gmail.com>
2020-10-31 16:26:01 +0530

    Used environmentobject to inject the viewmodel
    Signed-off-by: tejuamirthi <tejuamirthi@gmail.com>

--- SegmentedControl-SwiftUI/SwiftUIView.swift
+++ SegmentedControl-SwiftUI/SwiftUIView.swift
@@ -9,11 +9,10 @@
 
 import SwiftUI
 
-struct SegmentData: Identifiable {
-    var id = UUID()
+public struct Segment {
     var title: String?
-    var isSelected: Bool?
     var image: String?
+    var isSelected: Bool?
     
     init(title: String?, isSelected: Bool = false, image: String? = nil) {
         self.title = title
@@ -22,64 +21,88 @@ struct SegmentData: Identifiable {
     }
 }
 
-class SegmentDataViewModel: ObservableObject {
-    @Published var data: [SegmentData] = [
-    .init(title: "Title 1"),
+public struct SegmentsData {
+    var selectedIndex: Int {
+        didSet {
+            items[oldValue].isSelected = false
+            items[selectedIndex].isSelected = true
+        }
+    }
+    var items: [Segment]
+    
+    init(items: [Segment], index: Int) {
+        self.items = items
+        self.selectedIndex = index
+    }
+}
+
+public class SegmentDataViewModel: ObservableObject {
+    @Published var data = SegmentsData(items: [
+    .init(title: "Title 1", isSelected: true),
     .init(title: "Title 2"),
     .init(title: "Very large title 3"),
     .init(title: "Can't imagine this long title")
-    ]
+    ], index: 0)
+    
+    
+    func changeSelected(_ index: Int) {
+        data.selectedIndex = index
+    }
 }
 
 
-struct SegmentedControlView: View {
-    // let data: [String] = ["one", "Big big biiiiggg two", "three", "Very bery big four cant imagine big"]
-//    @State var isSelected: [Bool] = [true, false, true, false]
+public struct SegmentedControlView: View {
     
     @ObservedObject var segmentDataVM: SegmentDataViewModel = SegmentDataViewModel()
-    var body: some View {
-        VStack {
+    
+    public init() {
+        
+    }
+    
+    public var body: some View {
+        
+            VStack {
 
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack {
-//                    ForEach(Array(zip(data, isSelected)), id: \.0) { item in
-//                        RowDataView(item: item.0, isSelected: item.1)
-//                    }
-                    ForEach(segmentDataVM.data.indices) { index in
-                        RowDataView(item: index, data: self.segmentDataVM.data)
-                    }
-                }.aspectRatio(contentMode: ContentMode.fill)
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(segmentDataVM.data.items.indices) { index in
+                            RowDataView(data: self.segmentDataVM.data.items[index], index: index)
+                                .environmentObject(self.segmentDataVM)
+                        }
+                    }.aspectRatio(contentMode: ContentMode.fill)
+                }
+                Spacer()
             }
-            Text("This is supposed to be body")
-            Spacer()
-        }
-        .edgesIgnoringSafeArea(Edge.Set())
+        
     }
 }
 
-struct RowDataView: View {
-    var segmentData: [SegmentData]
+public struct RowDataView: View {
+    
+    @EnvironmentObject var vm: SegmentDataViewModel
+    
     let index: Int
     
-    init(item: Int, data: [SegmentData]) {
-        segmentData = data
-        index = item
+    init(data: Segment, index: Int ) {
+        self.index = index
+        
     }
     
-    var body: some View {
-//        Text(item)
+    func getSegment() -> Segment {
+        return vm.data.items[index]
+    }
+    
+    public var body: some View {
          let view: some View = RoundedRectangle(cornerRadius: 16)
             .stroke(Color.black, lineWidth: 2)
             
         return Button(action: {
-//            self.segmentData[self.index].isSelected?.toggle()
-        }) {
-            Text(segmentData[index].title ?? "Hey ya")
-        }
-        
-//        return Text(item)
+                self.vm.changeSelected(self.index)
+            }) {
+                Text(getSegment().title ?? "Hey ya")
+            }
             .padding()
-            .if(segmentData[index].isSelected ?? false, trueContent: {
+            .if(getSegment().isSelected ?? false, trueContent: {
                 $0.background(Color.black)
                 .foregroundColor(.white)
                 }, falseContent: {
@@ -87,34 +110,13 @@ struct RowDataView: View {
                 .foregroundColor(.black)
                 })
             .cornerRadius(16)
-            .if(!(segmentData[index].isSelected ?? false), trueContent: {
+            .if(!(getSegment().isSelected ?? false), trueContent: {
                 $0.overlay(view)
             })
-    .padding(EdgeInsets(top: 4, leading: 0, bottom: 4, trailing: 0))
-        
-        
-        
-        
-        
-        //.background(Color.blue.overlay(view))
-//            .overlay(view)
-            
-            
-        
-//        RoundedRectangle(cornerRadius: 25.0).fill(Color.blue)
-//            .aspectRatio(2.5, contentMode: .fit)
-//            .overlay(
-//                Text(item)
-//            )
+            .padding(EdgeInsets(top: 4, leading: 0, bottom: 4, trailing: 0))
         
     }
 }
-
-//struct RowDataView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        RowDataView(item: "Heylo")
-//    }
-//}
 
 struct SegmentedControlView_Previews: PreviewProvider {
     static var previews: some View {
@@ -123,7 +125,7 @@ struct SegmentedControlView_Previews: PreviewProvider {
 }
 
 
-extension View {
+public extension View {
 
     func `if`<Content: View>(_ conditional: Bool, trueContent: ((Self) -> Content)? = nil, falseContent: ((Self) -> Content)? = nil) -> some View {
         if conditional, let content = trueContent {
