6630e468e692b0f64151357cda02c82c4fce63ed
tejuamirthi <tejuamirthi@gmail.com>
2020-11-01 23:31:22 +0530

    Updated state management and binding with RowDataView

--- SegmentedControl-SwiftUI/SwiftUIView.swift
+++ SegmentedControl-SwiftUI/SwiftUIView.swift
@@ -9,112 +9,88 @@
 
 import SwiftUI
 
-public struct Segment {
+struct Segment {
     var title: String?
     var image: String?
-    var isSelected: Bool?
     
-    init(title: String?, isSelected: Bool = false, image: String? = nil) {
+    init(title: String?, image: String? = nil) {
         self.title = title
-        self.isSelected = isSelected
         self.image = image
     }
 }
 
-public struct SegmentsData {
-    var selectedIndex: Int {
-        didSet {
-            items[oldValue].isSelected = false
-            items[selectedIndex].isSelected = true
-        }
+public struct Segments {
+    public init() {
+        
     }
-    var items: [Segment]
-    
-    init(items: [Segment], index: Int) {
-        self.items = items
-        self.selectedIndex = index
-    }
-}
-
-public class SegmentDataViewModel: ObservableObject {
-    @Published var data = SegmentsData(items: [
-    .init(title: "Title 1", isSelected: true),
+    var data: [Segment] = [
+    .init(title: "Title 1"),
     .init(title: "Title 2"),
     .init(title: "Very large title 3"),
-    .init(title: "Can't imagine this long title")
-    ], index: 0)
-    
-    
-    func changeSelected(_ index: Int) {
-        data.selectedIndex = index
-    }
+    .init(title: "Can't imagine this long title"),
+        .init(title: "5th title too long")
+    ]
 }
-
 
 public struct SegmentedControlView: View {
     
-    @ObservedObject var segmentDataVM: SegmentDataViewModel = SegmentDataViewModel()
+    var segments: Segments = Segments()
+    @State var selectedIndex = 0
+    @Namespace var animation
     
     public init() {
         
     }
     
     public var body: some View {
-        
-            VStack {
-
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        ForEach(segmentDataVM.data.items.indices) { index in
-                            RowDataView(data: self.segmentDataVM.data.items[index], index: index)
-                                .environmentObject(self.segmentDataVM)
-                        }
-                    }.aspectRatio(contentMode: ContentMode.fill)
-                }
-                Spacer()
+        return VStack {
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(0..<self.segments.data.count) { index in
+                        RowDataView(selectedIndex: $selectedIndex, index: index, data: self.segments.data[index], animation: animation)
+                    }
+                }.aspectRatio(contentMode: ContentMode.fill)
             }
-        
+            Spacer()
+        }
     }
 }
 
-public struct RowDataView: View {
-    
-    @EnvironmentObject var vm: SegmentDataViewModel
-    
+struct RowDataView: View {
+    @Binding var selectedIndex: Int
     let index: Int
+    let data: Segment
+    let animation: Namespace.ID
     
-    init(data: Segment, index: Int ) {
-        self.index = index
-        
-    }
     
-    func getSegment() -> Segment {
-        return vm.data.items[index]
-    }
-    
-    public var body: some View {
-         let view: some View = RoundedRectangle(cornerRadius: 16)
+    var body: some View {
+        let view: some View = RoundedRectangle(cornerRadius: 16)
             .stroke(Color.black, lineWidth: 2)
-            
+
         return Button(action: {
-                self.vm.changeSelected(self.index)
-            }) {
-                Text(getSegment().title ?? "Hey ya")
+            withAnimation {
+                selectedIndex = index
             }
-            .padding()
-            .if(getSegment().isSelected ?? false, trueContent: {
-                $0.background(Color.black)
-                .foregroundColor(.white)
-                }, falseContent: {
-                    $0.background(Color.white)
-                .foregroundColor(.black)
-                })
-            .cornerRadius(16)
-            .if(!(getSegment().isSelected ?? false), trueContent: {
-                $0.overlay(view)
-            })
+            }) {
+                ZStack {
+                    if selectedIndex == index {
+                        Text(data.title ?? "Hey ya")
+                            .padding()
+                            .background(Color.black)
+                            .foregroundColor(.white)
+                            .cornerRadius(16)
+                            .matchedGeometryEffect(id: /*@START_MENU_TOKEN@*/"ID"/*@END_MENU_TOKEN@*/, in: animation)
+                    } else {
+                        Text(data.title ?? "Hey ya")
+                            .padding()
+                            .background(Color.white)
+                            .foregroundColor(.black)
+                            .cornerRadius(16)
+                            .overlay(view)
+                    }
+                }
+            }
             .padding(EdgeInsets(top: 4, leading: 0, bottom: 4, trailing: 0))
-        
     }
 }
 
